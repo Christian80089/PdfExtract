938b10dd2b65037f984798956492edbe3e9ce3a3
chris <christiandelprete01@gmail.com>
2025-01-25 12:43:00 +0100

    Push App

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
-import streamlit as st
 import plotly.express as px
+import streamlit as st
 
 # Percorso del file CSV nella cartella resources
 base_path = os.path.abspath(os.path.dirname(__file__))
@@ -10,11 +10,16 @@ csv_path = os.path.join(base_path, "../resources/output_data/salary/salary_histo
 # Carica il CSV
 def load_data():
     try:
-        data = pd.read_csv(csv_path)
-        return data
+        csv_data = pd.read_csv(csv_path)
+        return csv_data
     except FileNotFoundError:
         st.error("Il file CSV non è stato trovato. Verifica il percorso e riprova.")
         return None
+
+
+# Funzione per formattare i numeri con punto per le migliaia e virgola per i decimali
+def format_number(value):
+    return f"{value:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
 # Funzione principale
 st.title("Dashboard Analisi Stipendi")
@@ -31,9 +36,21 @@ if data is not None:
         [data['date_periodo_di_retribuzione'].min(), data['date_periodo_di_retribuzione'].max()]
     )
 
-    # Filtra i dati
-    filtered_data = data[(data['date_periodo_di_retribuzione'] >= pd.to_datetime(start_date)) &
-                         (data['date_periodo_di_retribuzione'] <= pd.to_datetime(end_date))]
+    # Filtro per ragione sociale azienda
+    st.sidebar.header("Filtra per azienda")
+    azienda_options = data['ragione_sociale_azienda'].unique()
+    azienda_selezionata = st.sidebar.selectbox(
+        "Seleziona Azienda",
+        azienda_options,
+        index=list(azienda_options).index("Relatech Spa")  # Imposta "Relatech Spa" come valore predefinito
+    )
+
+    # Filtra i dati in base al range di date e azienda
+    filtered_data = data[
+        (data['date_periodo_di_retribuzione'] >= pd.to_datetime(start_date)) &
+        (data['date_periodo_di_retribuzione'] <= pd.to_datetime(end_date)) &
+        (data['ragione_sociale_azienda'] == azienda_selezionata)
+    ]
 
     st.header("Statistiche generali")
 
@@ -44,23 +61,19 @@ if data is not None:
     media_ore_lavorate = filtered_data['ore_lavorate'].mean()
     totale_straordinari = filtered_data['ore_straordinarie'].sum()
     totale_irpef = filtered_data['irpef_pagata'].sum()
-    ferie_rimanenti = filtered_data['totale_ferie_rimanenti'].iloc[-1]
-    permessi_rimanenti = filtered_data['totale_permessi_rimanenti'].iloc[-1]
+    ferie_rimanenti = round(filtered_data['totale_ferie_rimanenti'].iloc[-1], 2)
+    permessi_rimanenti = round(filtered_data['totale_permessi_rimanenti'].iloc[-1], 2)
 
     # Visualizzazione delle metriche principali
     col1, col2, col3 = st.columns(3)
-    col1.metric("Totale Stipendio Netto", f"{totale_netto:,.2f} €")
-    col2.metric("Media Stipendio Netto", f"{media_netto:,.2f} €")
-    col3.metric("Totale Retribuzione Lorda", f"{totale_lordo:,.2f} €")
+    col1.metric("Totale Stipendio Netto", f"{format_number(totale_netto)} €")
+    col2.metric("Media Stipendio Netto", f"{format_number(media_netto)} €")
+    col3.metric("Totale Retribuzione Lorda", f"{format_number(totale_lordo)} €")
 
     col4, col5, col6 = st.columns(3)
-    col4.metric("Media Ore Lavorate", f"{media_ore_lavorate:,.2f}")
-    col5.metric("Totale Ore Straordinarie", f"{totale_straordinari:,.2f}")
-    col6.metric("Totale IRPEF Pagata", f"{totale_irpef:,.2f} €")
-
-    col7, col8 = st.columns(2)
-    col7.metric("Ferie Rimanenti", ferie_rimanenti)
-    col8.metric("Permessi Rimanenti", permessi_rimanenti)
+    col4.metric("Ferie Rimanenti (Giorni)", format_number(ferie_rimanenti))
+    col5.metric("Permessi Rimanenti (Ore)", format_number(permessi_rimanenti))
+    col6.metric("Totale IRPEF Pagata", f"{format_number(totale_irpef)} €")
 
     st.header("Grafico temporale dello stipendio netto")
 
@@ -73,19 +86,6 @@ if data is not None:
         labels={"date_periodo_di_retribuzione": "Periodo", "netto_del_mese": "Stipendio Netto"}
     )
     st.plotly_chart(fig)
-
-    st.header("Grafico temporale delle ore lavorate e straordinarie")
-
-    # Grafico temporale delle ore lavorate e straordinarie
-    fig2 = px.bar(
-        filtered_data,
-        x='date_periodo_di_retribuzione',
-        y=['ore_lavorate', 'ore_straordinarie'],
-        title='Confronto tra Ore Lavorate e Straordinarie',
-        labels={"value": "Ore", "date_periodo_di_retribuzione": "Periodo"},
-        barmode='group'
-    )
-    st.plotly_chart(fig2)
 
     st.header("Ripartizione tra competenze e trattenute")
 
